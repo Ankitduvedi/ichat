f2c07d533432b38ffc9a06ab26d6c0870a193890
Ankit kumar duvedi <ankitduvedi.03@gmail.com>
2022-10-16 22:37:05 +0530

    nodemon

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -4,7 +4,12 @@ const io = require('socket.io')(8000)
 const users ={}
 
 io.on('connection', socket =>{    //io.on is used to listen all connections 
-    socket.on('user-joined', name =>{   //socket.on is used to listen for new conections means whenever a new user is joined.
+    socket.on('new-user-joined', name =>{   //socket.on is used to listen for new conections means whenever a new user is joined.
         users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name); //it will broadcast message to every user except that one
+    })
+
+    socket.on('send', message =>{
+        socket.broadcast.emit('receive', {message: message, name: users[socket.id]})
     })
 })
