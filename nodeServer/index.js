753cfb7fd97f494c1df655946791aa728058d07d
Ankit kumar duvedi <ankitduvedi.03@gmail.com>
2022-10-16 21:35:09 +0530

    npm init

---
+++ nodeServer/index.js
@@ -0,0 +1,10 @@
+//Node server which handle socket io connections
+const io = require('socket.io')(8000)
+
+const users ={}
+
+io.on('connection', socket =>{
+    socket.on('user-joined', name =>{
+        users[socket.id] = name;
+    } )
+})
