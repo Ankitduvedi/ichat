717a31267246d8a992f7540b553e8604f47d18a9
Ankit kumar duvedi <ankitduvedi.03@gmail.com>
2022-10-17 00:49:23 +0530

    express

---
+++ js/client.js
@@ -0,0 +1,14 @@
+const socket = io('http://localhost:8000');
+
+const form = document.getElementById('send-container');
+const messageInput = document.getElementById('messageInp');
+const messagecontainer = document.querySelector(".container");
+
+const append = (message, position) =>{
+    const messageElement = document.createElement('div')
+    messageElement.innerText = message;
+    messageElement.classList.add('message')
+}
+const names = prompt("enter your name to join");
+
+socket.emit('new-user-joined', names)
--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,15 +1,28 @@
-//Node server which handle socket io connections
-const io = require('socket.io')(8000)
+//Node server which handle socket io connecti
+// const express = require('express');
+// const request = require('request');
 
+// const app = express();
+
+// app.use((req, res, next) => {
+//   res.header(Access-Control-Allow-Origin, '*');
+//   next();
+// });
+
+
+
+const io = require('socket.io')(8000)
 const users ={}
 
 io.on('connection', socket =>{    //io.on is used to listen all connections 
     socket.on('new-user-joined', name =>{   //socket.on is used to listen for new conections means whenever a new user is joined.
         users[socket.id] = name;
+        console.log(name)
         socket.broadcast.emit('user-joined', name); //it will broadcast message to every user except that one
     })
 
     socket.on('send', message =>{
         socket.broadcast.emit('receive', {message: message, name: users[socket.id]})
     })
-})
+});
+
